fdd02a36e0008834bb576805583c2ddb2b5ce03d
Kristin Lee <serpent.kris@gmail.com>
2019-09-26 21:24:22 -0700

    Basic working app with 2 views. No data editing through forms yet.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from "react-redux";
 import './App.css';
+import ListView from './views/ListView'
+import DetailView from './views/DetailView'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function ConnectedApp(props) {
+  if (props.detailView){
+    return <DetailView/>
+    return <p> hi </p>
+  }
+  else {
+    return <ListView/>
+  }
+
 }
 
-export default App;
+function mapStateToProps(state) {
+  return ({detailView: state.detailView})
+}
+
+const App = connect(mapStateToProps)(ConnectedApp)
+export default App
